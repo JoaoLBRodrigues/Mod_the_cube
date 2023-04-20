965e8fe1912c8d30121974e74bcfe2c98ed990fd
João Rodrigues <rodrigues2001mdl@gmail.com>
2023-04-20 15:00:30 +0100

    Initial commit

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 3);
+        transform.localScale = Vector3.one * 1.3f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.5f, 0.4f, 1.0f, 0.4f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(5.0f * Time.deltaTime, 2.0f, 0.0f);
+    }
+}
